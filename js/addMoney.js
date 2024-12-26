abb9adbb3c699ab3bcbffe87887d5f31f0261263
Your Name <shafiulhossain52@gmail.com>
2024-12-26 07:45:09 +0600

    account balance form added

---
+++ js/addMoney.js
@@ -0,0 +1,23 @@
+/**
+ *  how to get a number from an input field 
+ * 1. create a variable 
+ * 2. right side document.get ElementBY Id ()
+ * 3. value 
+ * 4. parsefloat 
+ * 
+ */
+
+
+
+document.getElementById('btn-add-Money')
+.addEventListener('click',function(event){
+
+event.preventDefault();
+
+
+//  const addMoney  = document.getElementById('input-add-money').value;
+//  const addMoneyNumber = parseFloat(addMoney);
+
+
+
+});
---
+++ js/utilities.js
@@ -0,0 +1,10 @@
+/**
+ * how to get a number from an 
+ * 
+ * 
+ * common shared functions here 
+ * 
+ */
+function getInputFieldValueById (){
+    console.log('will get value by id');
+}
